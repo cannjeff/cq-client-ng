1c98ec552b39c93b194c1d0ffaf525e050f48c67
Jeff Cann <cannjeff@gmail.com>
2015-06-24 06:31:12 -0400

    made the solving area much cleaner and added some niceties

--- src/app/components/quip/quipControllers.js
+++ src/app/components/quip/quipControllers.js
@@ -13,21 +13,26 @@ quipControllers.controller('QuipsSolveCtrl', [ '$scope', 'quips', '$routeParams'
 		scope.keyObject = _.object(scope.alphabet); /* keyObject is a terrible name, but its late and im tired */
 
 		/* Set the hint by default (lock it later?) */
-		scope.keyObject[ quip.hint.charAt(0).toUpperCase() ] = quip.hint.slice(-1).toUpperCase();
+		var hintKey = quip.hint.charAt(0).toUpperCase(),
+			hintVal = quip.hint.slice(-1).toUpperCase();
+		scope.keyObject[ hintKey ] = hintVal;
 
 		scope.updateMap = function ( key ) {
 			/* Force uppercase and cap at one letter */
 			scope.keyObject[ key ] = scope.keyObject[ key ].slice(-1).toUpperCase();
-			
+
 			/* Check for duplicates and replace the old ones */
 			var val = scope.keyObject[ key ];
-			if (val.length) {
+			if (val.length && val !== hintVal) {
 				_(scope.alphabet).each(function ( letter ) {
 					/* Exclude the current key. If any others match up clear them */
 					if (letter !== key && scope.keyObject[ letter ] === val) {
 						scope.keyObject[ letter ] = '';
 					}
 				});
+			} else if (val === hintVal) {
+				/* Don't allow them to override the hint */
+				scope.keyObject[ key ] = '';
 			}
 		};
 
@@ -47,7 +52,10 @@ quipControllers.controller('QuipsSolveCtrl', [ '$scope', 'quips', '$routeParams'
 					return /^[a-zA-Z]+$/.test( letter );
 				},
 				isPunctuation: function () {
-					return /^[\.,"':;]$/.test( letter );
+					return /^[\.,"':;?]$/.test( letter );
+				},
+				isHint: function () {
+					return letter === hintKey;
 				}
 			};
 		});
